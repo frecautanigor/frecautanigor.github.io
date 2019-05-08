304153f21ffa0369fc361e18cafec5f4877164fb
danielpascari <vajno.1997@mail.ru>
2019-05-08 11:16:23 +0300

    commit 2

--- test/js/common.js
+++ test/js/common.js
@@ -504,4 +504,20 @@ $( document ).ready(function() {
 		autoplaySpeed: 5000,
 	});
 
+	// fb
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      xfbml            : true,
+	      version          : 'v3.2'
+	    });
+	  };
+
+	  (function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/ru_RU/sdk/xfbml.customerchat.js';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	  
 });
