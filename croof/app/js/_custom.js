63139c451c11d5baa24af124f9fedd0d9bcad3f3
Igor Frecautan <frecautan.igor@gmail.com>
2019-08-19 11:01:21 +0300

    testtttt

--- croof/app/js/_custom.js
+++ croof/app/js/_custom.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	$('.sliderr').slick({
+	$('.slider_content').slick({
 		dots: true,
 		prevArrow: false,
     	nextArrow: false,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
-		adaptiveHeight: true
+		adaptiveHeight: true,
+		autoplay: true
 	});
+
+	// Fixed menu
+	var waypoint = new Waypoint({
+		element: document.getElementsByClassName('header_menu'),
+		handler: function(dir) {
+		  if (dir === 'down') {
+			$('.header_menu').addClass('fixed animated fadeInDown');
+		  } else
+		  $('.header_menu').removeClass('fixed animated fadeInDown');
+		}
+	  });
 
 });
